b053e9ad521cd65bdde9f80c867f5bc87b82ea6d
Zike <144278129+Ekiz24@users.noreply.github.com>
2023-11-08 15:40:38 +0200

    Add files via upload

---
+++ Assets/Scripts/SpeedRacer.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SpeedRacer : MonoBehaviour
+{
+    public string carMaker;
+    public string carModel = "GTR R35";
+    public string engineType = "V6, Twin Turbo";
+
+    public int carWeight = 1609;
+    public int yearMade = 2009;
+
+    public float maxAcceleration = 0.98f;
+
+    public bool isCarTypeSedan = false;
+    public bool hasFrontEngine = true;
+
+
+    void Start()
+    {
+        print("The racer model is " + carModel + " by " + carMaker + ". It has a " + engineType + " engine.");
+
+        CheckWeight();
+
+        if (yearMade <= 2009)
+        {
+            print("It was first introduced in " + yearMade);
+            int carAge = CalculateAge(yearMade);
+            print("That makes it " + carAge + " years old.");
+        }
+        else
+        {
+            print("It was introduced in the 2010's");
+            print("And its maximum acceleration is " + maxAcceleration + " m/s2");
+        }
+
+        print(CheckCharacteristics());
+    }
+
+    void CheckWeight()
+    {
+        if (carWeight < 1500)
+        {
+            print("The " + carModel + " weighs less than 1500 kg.");
+        }
+        else
+        {
+            print("The " + carModel + " weighs over 1500 kg.");
+        }
+    }
+
+    int CalculateAge(int yearMade)
+    {
+        return 2023 - yearMade;
+    }
+
+    string CheckCharacteristics()
+    {
+        if (isCarTypeSedan)
+        {
+            return "The car is a sedan type.";
+        }
+        else if (hasFrontEngine)
+        {
+            return "The car is not a sedan, but has a front engine.";
+        }
+        else
+        {
+            return "The car is neither a sedan, nor is its engine a front engine.";
+        }
+    }
+}
